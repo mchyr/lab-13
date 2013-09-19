9fcb91b9030c9454fef24e7266de3cf1516516f5
Christopher Lindsay <christopher.lindsay7@gmail.com>
2013-09-18 22:28:54 -0400

    try again

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ActiveRecord::Base
   
   belongs_to: category
+  
+  scope :active, where('active = ?', true)
+  scope :alphabetical, order('title')
+  
+  validates_presence_of :title
+  validates_presence_of :content 
 end
